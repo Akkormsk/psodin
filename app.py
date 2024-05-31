732424c52c1c2e8c57838f9bd7ec8d62e99b2ce0
akkormsk <akkormsk@gmail.com>
2024-05-31 22:13:50 +0400

    Первый рабочий прототип с миграцией

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
-from models import db, PaperType, PrintType
+from models import db, PaperType, PrintType, init_db
+from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS, SECRET_KEY
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///print_shop.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Отключаем отслеживание изменений
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+app.config['SECRET_KEY'] = SECRET_KEY
 db.init_app(app)
 migrate = Migrate(app, db)
 
@@ -28,7 +29,7 @@ class CalculatorView(BaseView):
         quantity = int(request.form['quantity'])
         paper_type = PaperType.query.get(paper_type_id)
         print_type = PrintType.query.get(print_type_id)
-        total_price = quantity * (paper_type.price_per_unit + print_type.price_per_unit)
+        total_price = round(quantity * (paper_type.price_per_unit + print_type.price_per_unit), 2)
         paper_types = PaperType.query.all()
         print_types = PrintType.query.all()
         return self.render('admin/calculator.html', total_price=total_price, paper_types=paper_types,
@@ -41,26 +42,9 @@ admin.add_view(ModelView(PrintType, db.session))
 admin.add_view(CalculatorView(name='Calculator', endpoint='calculator'))
 
 
-def init_db():
-    with app.app_context():
-        db.create_all()
-        if PaperType.query.count() == 0:
-            paper_types = [
-                {"name": "Standard", "price_per_unit": 0.10},
-                {"name": "Premium", "price_per_unit": 0.20}
-            ]
-            for paper_type in paper_types:
-                db.session.add(PaperType(name=paper_type['name'], price_per_unit=paper_type['price_per_unit']))
-            db.session.commit()
-
-        if PrintType.query.count() == 0:
-            print_types = [
-                {"name": "Digital", "price_per_unit": 0.05},
-                {"name": "Offset", "price_per_unit": 0.15}
-            ]
-            for print_type in print_types:
-                db.session.add(PrintType(name=print_type['name'], price_per_unit=print_type['price_per_unit']))
-            db.session.commit()
+@app.route('/')
+def index():
+    return redirect('/admin')
 
 
 if __name__ == '__main__':
@@ -68,8 +52,3 @@ if __name__ == '__main__':
     with app.app_context():
         init_db()
     app.run(debug=True)
-
-
-@app.route('/')
-def index():
-    return redirect('/admin')
