0107863ea1c590efe9a6a7e13a5f7fda78d12793
akkormsk <akkormsk@gmail.com>
2024-06-02 15:38:55 +0400

    Попытка визуализации меню

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
-from models import db, PaperType, PrintType, init_db
-from config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS, SECRET_KEY
+from models import db, init_db, PaperType, PrintType
+
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config.from_pyfile('config.py')
 db.init_app(app)
 migrate = Migrate(app, db)
 
@@ -31,7 +31,8 @@ class CalculatorView(BaseView):
             error_message = "Введите значение в поле количество"
             paper_types = PaperType.query.all()
             print_types = PrintType.query.all()
-            return self.render('admin/calculator.html', error_message=error_message, paper_types=paper_types, print_types=print_types)
+            return self.render('admin/calculator.html', error_message=error_message, paper_types=paper_types,
+                               print_types=print_types)
 
         quantity = int(quantity)
         paper_type = PaperType.query.get(paper_type_id)
@@ -39,12 +40,14 @@ class CalculatorView(BaseView):
         total_price = round(quantity * (paper_type.price_per_unit + print_type.price_per_unit), 2)
         paper_types = PaperType.query.all()
         print_types = PrintType.query.all()
-        return self.render('admin/calculator.html', total_price=total_price, paper_types=paper_types, print_types=print_types)
+        return self.render('admin/calculator.html', total_price=total_price, paper_types=paper_types,
+                           print_types=print_types)
 
 
-admin = Admin(app, name='Print Shop Admin', template_mode='bootstrap3', base_template='base.html')
-admin.add_view(ModelView(PaperType, db.session))
-admin.add_view(ModelView(PrintType, db.session))
+# Initialize admin panel
+admin = Admin(app, name='Print Shop Admin', template_mode='bootstrap3')
+admin.add_view(ModelView(PaperType, db.session, endpoint='papertype'))
+admin.add_view(ModelView(PrintType, db.session, endpoint='printtype'))
 admin.add_view(CalculatorView(name='Calculator', endpoint='calculator'))
 
 
@@ -59,7 +62,6 @@ def admin_index():
 
 
 if __name__ == '__main__':
-    # Выполняем инициализацию базы данных при запуске приложения
     with app.app_context():
         init_db()
     app.run(debug=True)
