ed3470803f6a2452c89f21886b5693d489144bb3
akkormsk <akkormsk@gmail.com>
2024-06-03 13:16:30 +0400

    чистка папок

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from models import db, PaperType, PrintType
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app)
 
-
-class PaperType(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price_per_unit = db.Column(db.Float, nullable=False)
-
-
-class PrintType(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    price_per_unit = db.Column(db.Float, nullable=False)
-
+# Flask-Admin
+admin = Admin(app, name='PSadmin Panel', template_mode='bootstrap3')
+admin.add_view(ModelView(PaperType, db.session))
+admin.add_view(ModelView(PrintType, db.session))
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 @app.route('/admin/calculator/', methods=['GET', 'POST'])
 def calculator():
@@ -38,9 +31,7 @@ def calculator():
         paper_type = PaperType.query.get(paper_type_id)
         print_type = PrintType.query.get(print_type_id)
         total_cost = round((paper_type.price_per_unit + print_type.price_per_unit) * quantity, 2)
-    return render_template('admin/calculator.html', paper_types=paper_types, print_types=print_types,
-                           total_cost=total_cost)
-
+    return render_template('admin/calculator.html', paper_types=paper_types, print_types=print_types, total_cost=total_cost)
 
 if __name__ == '__main__':
     with app.app_context():
