c2d674c995ca07ced95cf05a455985ca2358cdfb
akkormsk <akkormsk@gmail.com>
2024-06-03 14:28:49 +0400

    чистка папок

--- models.py
+++ models.py
@@ -2,10 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class PaperType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price_per_unit = db.Column(db.Float, nullable=False)
+
 
 class PrintType(db.Model):
     id = db.Column(db.Integer, primary_key=True)
