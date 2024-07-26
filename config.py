a40c9642705813726e9ec31cc0bdf32d3846a77a
akkormsk <akkormsk@gmail.com>
2024-07-26 21:54:14 +0400

    Переход на MySQL

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'print_shop.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'print_shop.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://user1:@localhost/psodin_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '1111'
     SECRET_ADMIN_PASSWORD = '1111'  # Установите ваш пароль здесь
--- models.py
+++ models.py
@@ -2,58 +2,50 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class PaperType(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     price_per_unit = db.Column(db.Float, nullable=False)
-
 
 class PaperTypeLarge(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     price_per_unit = db.Column(db.Float, nullable=False)
 
-
 class PrintType(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     price_per_unit_xerox = db.Column(db.Float, nullable=False)
     price_per_unit_konica = db.Column(db.Float, nullable=False)
 
-
 class PrintTypeLarge(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     price_per_unit_canon = db.Column(db.Float, nullable=False)
 
-
 class PostPrintProcessing(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     price_per_unit = db.Column(db.Float, nullable=False)
-
 
 class PostPrintProcessingLarge(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     price_per_unit = db.Column(db.Float, nullable=False)
-
 
 class Embossing(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     price_per_unit = db.Column(db.Float, nullable=False)
 
-
 class Variables(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
     value = db.Column(db.Float, nullable=False)
 
-
 class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'orders'  # Обновлено название таблицы
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     retail_price = db.Column(db.Float, nullable=False)
     cost_price = db.Column(db.Float, nullable=False)
-    materials = db.Column(db.String, nullable=False)
+    materials = db.Column(db.String(500), nullable=False)  # Указание длины
