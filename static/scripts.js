63ff0fa4d1ddbf1b873c83882ac07563d525b70f
akkormsk <akkormsk@gmail.com>
2024-08-19 12:39:37 +0400

    Исправлен баг калькулятора

--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Обновление цен при загрузке страницы и при смене типа принтера
+    document.querySelectorAll('select[name="machine_type"]').forEach(function(selectElement) {
+        updatePrintOptions(selectElement.value, selectElement);
+        selectElement.addEventListener('change', function() {
+            updatePrintOptions(this.value, this);
+        });
+    });
+});
+
+// Функция для обновления опций печати в зависимости от выбранного принтера
+function updatePrintOptions(machineType, element) {
+    const parentDiv = element.closest('.form-group');
+    const printSelect = parentDiv.querySelector('select[name="print_type"]');
+
+    // Обновление цен в зависимости от выбранной машины
+    printSelect.querySelectorAll('option').forEach(function(option) {
+        const xeroxPrice = option.getAttribute('data-xerox');
+        const konicaPrice = option.getAttribute('data-konica');
+        const price = machineType === 'xerox' ? xeroxPrice : konicaPrice;
+        option.textContent = `${option.textContent.split('-')[0].trim()} - ${price} руб/лист`;
+    });
+}
+
 function addPrintSection() {
     const template = document.getElementById('print-section-template');
     const clone = template.content.cloneNode(true);
@@ -6,49 +30,6 @@ function addPrintSection() {
 
     const newMachineSelect = printSections.lastElementChild.querySelector('select[name="machine_type"]');
     updatePrintOptions(newMachineSelect.value, newMachineSelect);
-
-    const newPrintContent = printSections.lastElementChild.querySelector('[id^=print-content-template]');
-    updatePrintPrices(newMachineSelect.value, newPrintContent);
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('select[name="machine_type"]').forEach(function(select) {
-        updatePrintOptions(select.value, select);
-    });
-
-    document.querySelectorAll('select[name="print_type"]').forEach(function(select) {
-        const machineType = select.closest('.form-group').querySelector('select[name="machine_type"]').value;
-        updatePrintPrices(machineType, select.closest('[id^=print-content]'));
-    });
-});
-
-function updatePrintOptions(machineType, element) {
-    const parentDiv = element.closest('.form-group');
-    const contentDiv = parentDiv.querySelector('[id^=print-content]');
-
-    fetch(`/update_print_options?machine_type=${machineType}`)
-        .then(response => response.json())
-        .then(options => {
-            let optionsHtml = '';
-            options.forEach(option => {
-                optionsHtml += `<option value="${option.id}" data-xerox="${option.price}" data-konica="${option.price}">
-                                    ${option.name} - ${option.price} руб/лист
-                                </option>`;
-            });
-            contentDiv.querySelector('select[name="print_type"]').innerHTML = optionsHtml;
-            updatePrintPrices(machineType, contentDiv);
-        });
-}
-
-
-function updatePrintPrices(machineType, contentDiv) {
-    contentDiv.querySelectorAll('option').forEach(function(option) {
-        if (machineType === 'xerox') {
-            option.innerText = `${option.innerText.split('-')[0]} - ${option.getAttribute('data-xerox')} руб/лист`;
-        } else if (machineType === 'konica') {
-            option.innerText = `${option.innerText.split('-')[0]} - ${option.getAttribute('data-konica')} руб/лист`;
-        }
-    });
 }
 
 function addPaperSection() {
