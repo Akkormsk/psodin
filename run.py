1d62587ffc2e422345f61ee91c8ffc1720dbb292
akkormsk <akkormsk@gmail.com>
2024-08-21 13:57:10 +0400

    правка порта

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        port = int(os.environ.get('PORT', 5000))  # Получаем порт из переменной окружения
+        app.run(host="0.0.0.0", port=port)  # Используем этот порт для запуска приложения
