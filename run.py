3f8f21d6a375aa121a490efc2eb25efe82f8eb1c
akkormsk <akkormsk@gmail.com>
2024-08-21 14:50:06 +0400

    пробую порт 3060

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
-import os
+
 from app import app
 
 if __name__ == '__main__':
     with app.app_context():
-        port = int(os.environ.get('PORT', 5000))  # Получаем порт из переменной окружения
-        app.run(host="0.0.0.0", port=5000)  # Используем этот порт для запуска приложения
+        app.run(host="0.0.0.0", port=3060)  # Используем этот порт для запуска приложения
