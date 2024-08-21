98cb49f7549969d0cdee9ebee840d1dfc4b19b46
akkormsk <akkormsk@gmail.com>
2024-08-21 15:15:15 +0400

    закомментил ран

--- app/__init__.py
+++ app/__init__.py
@@ -61,3 +61,4 @@ def log_user_action():
 if __name__ == '__main__':
     with app.app_context():
         app.run()
+        pass
--- run.py
+++ run.py
@@ -3,4 +3,5 @@ from app import app
 
 if __name__ == '__main__':
     with app.app_context():
-        app.run()
+        # app.run()
+        pass
